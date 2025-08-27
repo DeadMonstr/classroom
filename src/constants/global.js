9ecc49a996f20775f14a3eda2d9d3e2d5077b03c
null <raymuruvsanomuru@gmail.com>
2025-08-27 14:51:51 +0500

    pisaTestResults

--- src/constants/global.js
+++ src/constants/global.js
@@ -1,13 +1,14 @@
 import {useMemo} from "react";
 
 
-export const BackUrlForDoc = "http://26.253.30.50:5001/"
+// export const BackUrlForDoc = "http://26.253.30.50:5001/"
+export const BackUrlForDoc = "http://26.196.249.247:5001/"
 // export const BackUrlForDoc = "https://classroom.gennis.uz/"
 export const BackUrl = `${BackUrlForDoc}api/`
 export const LogoutUrl = "/login"
 export const LogoutUrlPisa = "/loginPisa"
 // export const PlatformUrl = "https://classroom.gennis.uz/"
-export const PlatformUrl = "http://26.253.30.50:5001/"
+export const PlatformUrl = "http://26.196.249.247:5001/"
 export const PlatformUrlApi = `${PlatformUrl}api/`
 
 
